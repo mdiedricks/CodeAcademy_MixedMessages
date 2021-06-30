e6d8844d422791d5a1dc011b68fb384702d7fa22
Michael <michael.diedricks@gmail.com>
2021-06-30 21:19:53 +1000

    Updated with proper message structure

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Mixed messages");
+function randomNum(num){
+	return Math.floor(Math.random()*num);	
+}
+
+const honeyPot = {
+	spiritAnimal:['Jack Black','Hagrid','Vladimir Putin', 'Spongebob Squarepants','Lisa Simpson'],
+	actionableInsight:['polish your golf clubs','dig a hole','collect some banana peels','lick a barnacle','hunt leprechauns'],
+	unforgivingOutcomes:['eternal hangnail','a duck shit on your lap','sweaty socks','ear fungus','baby chicken in your fried egg']
+};
+
+let eternalWisdom = [];
+
+function populateWisdom(){
+	for(let param in honeyPot){
+		let selectIndex = randomNum(honeyPot[param].length);
+		if(param == 'spiritAnimal'){
+			eternalWisdom.push(`Today your spirit animal is ${honeyPot[param][selectIndex]}.`);	
+		};
+		
+		if(param == 'actionableInsight'){
+			eternalWisdom.push(`It is advisable that you ${honeyPot[param][selectIndex]},`);	
+		};
+
+		if(param == 'unforgivingOutcomes'){
+			eternalWisdom.push(`lest you be plagued with ${honeyPot[param][selectIndex]}.`);
+		};
+	}
+};
+
+console.log(`Peering into the eternal yoghurt....hmmm.....what have we here?`);
+console.log(`: : ... :" : ... : :>, :, : ,:, :<`);
+populateWisdom();
+console.log(eternalWisdom.join(' '));
